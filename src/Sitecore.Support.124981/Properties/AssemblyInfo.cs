c00376c748d80692f9faf0e3924cd8a44f35bf96
Ivan Sharamok <isharamok@gmail.com>
2016-11-04 19:51:27 -0700

    Initial commit after project setup

---
+++ src/Sitecore.Support.124981/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.124981")]
+[assembly: AssemblyProduct("Sitecore.Support.124981")]
+[assembly: ComVisible(false)]
